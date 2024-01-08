c81bf49299b32a73e28efb5544abfbc57b9d11ff
gunapalani <gunapalani19@outlook.com>
2024-01-08 16:40:30 +0530

    feat: added initial setp genarate cli

---
+++ src/components/compentency/compentency.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './compentency.scss';
+
+interface CompentencyProps {}
+
+const Compentency: FC<CompentencyProps> = () => (
+  <div className="compentency" data-testid="Compentency">
+    Compentency Component
+  </div>
+);
+
+export default Compentency;
