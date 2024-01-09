ed08d3e702cfe9ef07eae3bdf56176d8a7585f59
Abirami Duraipandian <abirami@theproindia.com>
2024-01-09 11:46:54 +0530

    feat: routing added

---
+++ src/layout/Layout/Layout.tsx
@@ -0,0 +1,33 @@
+import "./Layout.scss";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { FC } from "react";
+
+import Compentency from "../../components/compentency/compentency";
+import SideBar from "../../components/SideBar/SideBar";
+
+interface LayoutProps {}
+
+const Layout: FC<LayoutProps> = () => (
+	<div className="Layout">
+		<header>
+			<img src="../assets/images/product-logo.svg" alt="logo" />
+		</header>
+		<div className="main">
+			<BrowserRouter>
+				<SideBar />
+				<main className="body">
+					<Routes>
+						<Route path="/" element={<Compentency />} />
+						<Route
+							path="/information/personal-info"
+							element={<Compentency />}
+						/>
+					</Routes>
+				</main>
+			</BrowserRouter>
+		</div>
+	</div>
+);
+
+export default Layout;
