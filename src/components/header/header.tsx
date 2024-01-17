3df539b323eea283c545b85ae3c0326577102b7a
Abirami Duraipandian <abirami@theproindia.com>
2024-01-17 10:52:52 +0530

    logo update

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,9 +9,12 @@ interface HeaderProps {}
 const Header: FC<HeaderProps> = () => (
 	<div className="header" data-testid="Header">
 		<header>
-			<img src="../assets/images/product-logo.svg" alt="logo" />
+			<div className="logo-name">
+				<img className="logo" src="../assets/images/flag.png" alt="logo" />
+				<span>Reward Rally</span>
+			</div>
 			<ProGamification
-				userId={"cef96b5a-2ad9-4c3d-aae1-eb6f9da6e5a8"}
+				userId={"b171b656-039d-4243-bfe7-1c8c9af6665c"}
 				applicationId={"659645cdf65b394e53135964"}
 				clientId={"08ce0068-2906-44d4-b840-b268e5511624"}
 				clientSecret={"2BG8Q~Vsd33.QQ93jcejZkrr5aVyXvxbl_FrubYg"}
