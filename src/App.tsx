c09aaee08e71a3710bf68167a1a6189c1bb58a8d
gunapalani <gunapalani19@outlook.com>
2024-01-17 18:09:42 +0530

    feat: main branch code remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import "./App.css";
-
-import Layout from "./layout/Layout/Layout";
-import React from "react";
-import logo from "./logo.svg";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<Layout />
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
