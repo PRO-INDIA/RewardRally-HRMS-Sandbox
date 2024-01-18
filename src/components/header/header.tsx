caee4d541ebb4a5273edd8b77d8667964b6e0d15
gunapalani <gunapalani19@outlook.com>
2024-01-18 12:33:20 +0530

    feat: bug fixes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -13,12 +13,14 @@ const Header: FC<HeaderProps> = () => (
         <img className="logo" src="../assets/images/flag.png" alt="logo" />
         <span>Reward Rally</span>
       </div>
-      <ProGamification
-        userId={environment.gamification.userId}
-        applicationId={environment.gamification.applicationId}
-        clientId={environment.clientId}
-        clientSecret={environment.clientSecret}
-      />
+      <div className="gamification-package">
+        <ProGamification
+          userId={environment.gamification.userId}
+          applicationId={environment.gamification.applicationId}
+          clientId={environment.clientId}
+          clientSecret={environment.clientSecret}
+        />
+      </div>
     </header>
   </div>
 );
