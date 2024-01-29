3bbfc50c58dc8d1790d16742013acb8b9f8d8aca
Gunasekaran19 <113411758+Gunasekaran19@users.noreply.github.com>
2024-01-29 17:36:46 +0530

    Client secret changes
    Client secret changes

--- src/Environments/Environment.ts
+++ src/Environments/Environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     clientId: "08ce0068-2906-44d4-b840-b268e5511624",
-    clientSecret: "2BG8Q~Vsd33.QQ93jcejZkrr5aVyXvxbl_FrubYg",
+    clientSecret: "CL68Q~D5jO5XEXI8TodHLkha3HG5E83zgZ6KmbZK",
     gamification: {
       userId: "c59c355c-bb89-44d1-87b6-87aa4239c11b",
       gameId: "65a76ee876b3478697f939e3",
